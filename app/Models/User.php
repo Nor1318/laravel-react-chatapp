715ca29735249f9d7fb05c691e00bd9855fb2013
Bobby Dahal <bobby@thefireflytech.com>
2026-03-24 21:10:59 +0545

    Create model, migrations and factories

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['avatar', 'name', 'email', 'email_verified_at', 'password', 'is_admin'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,5 +28,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function group()
+    {
+        return $this->belongsToMany(Group::class, 'group_users');
     }
 }
